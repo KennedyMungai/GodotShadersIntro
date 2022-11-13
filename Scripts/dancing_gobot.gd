57d109c0ee02e38b97db8b26e8c896acdc236a36
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-13 19:16:32 +0300

    Wired up the amplitude changes inside the amplitude_changed signal call

--- Scripts/dancing_gobot.gd
+++ Scripts/dancing_gobot.gd
@@ -7,3 +7,8 @@ onready var amplitude = material.get_shader_param("amplitude")
 func _ready() -> void:
 	print(amplitude)
 	assert(amplitude != null)
+
+
+func _on_AmplitudeController_amplitude_changed(value) -> void:
+	amplitude.x = value
+	material.set_shader_param("amplitude", amplitude)	
